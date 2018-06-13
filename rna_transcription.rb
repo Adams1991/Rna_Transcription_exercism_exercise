a6086ba5585b9dd7d95c607ff50027f578dcd2c2
Adams1991 <adams121@hotmail.co.uk>
2018-06-13 08:32:21 +0100

    6 of nine tests passing

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,8 +1,16 @@
 class Complement
 
+  def self.of_dna(dna_strand)
+  dna_strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')    #=> "h3ll*"
+  end
 
 end
 
 module BookKeeping
 VERSION = 4
 end
+
+# * `G` -> `C`
+# * `C` -> `G`
+# * `T` -> `A`
+# * `A` -> `U`
