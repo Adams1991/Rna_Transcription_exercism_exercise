4899f2ccd45794160a04d0db1abe9726e2ee04f0
Adams1991 <adams121@hotmail.co.uk>
2018-06-13 08:49:44 +0100

    exercise complete

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,11 +1,13 @@
 class Complement
 
   def self.of_dna(dna_strand)
-    return '' if !dna_strand.match?(/[GCTA]/)
+  return '' if !dna_strand.match(/^[GCTA]+$/)
   dna_strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
   end
 
 end
+
+
 
 module BookKeeping
 VERSION = 4
