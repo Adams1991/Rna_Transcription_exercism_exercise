b6f278597899f6dc4702a0abda14bed28de16dfd
Adams1991 <adams121@hotmail.co.uk>
2018-06-13 08:37:50 +0100

    8 0f 9 tests passing

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,7 +1,8 @@
 class Complement
 
   def self.of_dna(dna_strand)
-  dna_strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')    #=> "h3ll*"
+    return '' if !dna_strand.match?(/[GCTA]/)
+  dna_strand.gsub(/[GCTA]/, 'G' => 'C', 'C' => 'G', 'T' => 'A', 'A' => 'U')
   end
 
 end
